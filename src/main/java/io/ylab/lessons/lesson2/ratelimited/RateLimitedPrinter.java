94d26e7390e1a965dba8dba14b9131187eefb8ca
Angelina <angelina-262695@mail.ru>
2023-03-11 20:46:50 +0300

    task RateLimitedPrinter

---
+++ src/main/java/io/ylab/lessons/lesson2/ratelimited/RateLimitedPrinter.java
@@ -0,0 +1,28 @@
+package io.ylab.lessons.lesson2.ratelimited;
+
+public class RateLimitedPrinter {
+
+    private long lastTimeToPrint;
+    private int interval;
+
+    public RateLimitedPrinter(int interval) {
+        this.interval = interval;
+        this.lastTimeToPrint = 0;
+
+    }
+    public void print(String message) {
+        if (canPrinting()) {
+            System.out.println(message);
+            this.lastTimeToPrint = System.currentTimeMillis();
+        }
+    }
+
+    private boolean canPrinting() {
+        long currentInterval = System.currentTimeMillis() - this.lastTimeToPrint;
+        if (currentInterval > this.interval) {
+            return true;
+        }
+        return false;
+    }
+
+}
