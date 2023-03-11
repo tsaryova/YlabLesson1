324de0f9d768274a08876552e2c6e4a29f99e271
Angelina <angelina-262695@mail.ru>
2023-03-11 18:18:02 +0300

    task Sequences corrects

--- src/main/java/io/ylab/lessons/lesson2/sequences/NameRule.java
+++ src/main/java/io/ylab/lessons/lesson2/sequences/NameRule.java
@@ -3,8 +3,12 @@ package io.ylab.lessons.lesson2.sequences;
 public enum NameRule {
     A("a"), B("b"), C("c"), D("d"), E("e"), F("f"), G("g"), H("h"), I("i"), J("j");
     private String name;
+
     NameRule(String name) {
         this.name = name;
     }
-    public String getName() {return name;}
+
+    public String getName() {
+        return name;
+    }
 }
